babc06654463dfcdde698ee1204cd709d6e57033
Renee Paczkowski <rpaczkowski@gmail.com>
2022-05-07 09:28:20 -0400

    Added deleteSubmissionByIndex and Name functions

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const submissions = [
   { name: "Jill", score: 88, date: "2020-04-22", passed: true },
 ];
 
+// Function declaration vs expression
+
 function addSubmission(array, newName, newScore, newDate) {
   const newScoreSubmission = {
     // why does the color change like that sometimes?
@@ -14,4 +16,14 @@ function addSubmission(array, newName, newScore, newDate) {
     passed: newScore > 60 ? true : false,
   };
   array.push(newScoreSubmission); // How are array and submissions related?
+}
+
+function deleteSubmissionByIndex(array, index) {
+  array.splice(index, 1); // I'm not sure how indexes work still
+}
+
+// Not sure about this one...
+function deleteSubmissionByName(array, name) {
+  const index = array.findIndex((submissions) => submissions.name === name);
+  array.splice(index, 1);
 }
