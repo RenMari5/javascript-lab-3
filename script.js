9ad9584e9f7f5231adb5e4894b1af7c7c597afe5
Renee Paczkowski <rpaczkowski@gmail.com>
2022-05-07 09:12:25 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const submissions = [
+  { name: "Jane", score: 95, date: "2020-01-24", passed: true },
+  { name: "Joe", score: 77, date: "2018-05-14", passed: true },
+  { name: "Jack", score: 59, date: "2019-07-05", passed: false },
+  { name: "Jill", score: 88, date: "2020-04-22", passed: true },
+];
+
+function addSubmission(array, newName, newScore, newDate) {}
